69af59cee9fb7ede090c46f9d7351ee194dfe02f
doa379 <doa379@gmail.com>
2019-12-01 21:40:37 +0000

    ;

--- pa_list.c
+++ pa_list.c
@@ -30,8 +30,8 @@ void list_clear(list_t *list)
 void *list_next(list_t *list, void *data)
 {
   for (size_t i = list->count; i > 0; i--)
-    if ((char *) list->data + (i - 1) * list->el_size == data)
-      return (char *) list->data + i * list->el_size;
+    if ((char *) list + sizeof *list + (i - 1) * list->el_size == data)
+      return (char *) list + sizeof *list + i * list->el_size;
 
   return NULL;
 }
@@ -39,8 +39,8 @@ void *list_next(list_t *list, void *data)
 void *list_prev(list_t *list, void *data)
 {
   for (size_t i = list->count; i > 0; i--)
-    if ((char *) list->data + (i - 1) * list->el_size == data)
-      return (char *) list->data + (i - 2) * list->el_size;
+    if ((char *) list + sizeof *list + (i - 1) * list->el_size == data)
+      return (char *) list + sizeof *list + (i - 2) * list->el_size;
 
   return NULL;
 }
@@ -58,10 +58,10 @@ void *list_itr_head(list_t *list, size_t n)
   if (list == NULL || !list->count)
     return NULL;
 
-  else if (n > list->count)
-    return list_tail(list);
+  else if (n > list->count - 1)
+    return NULL;
   
-  return (char *) list->data + (n * list->el_size);
+  return (char *) list + sizeof *list + (n * list->el_size);
 }
 
 void *list_tail(list_t *list)
@@ -74,8 +74,8 @@ void *list_tail(list_t *list)
 
 void *list_itr_tail(list_t *list, size_t n)
 {
-  if (n > list->el_size)
-    return list_head(list);
+  if (n > list->count - 1)
+    return NULL;
   
   return (char *) list_tail(list) - n * list->el_size;
 }
@@ -89,13 +89,12 @@ void list_add(list_t *list, void *data)
 {
   if (list->count > list->alloc_size - 1)
     {      
-      if ((list->data = realloc(list->data, (list->alloc_size + LIST_INC_SIZE) * list->el_size)) == NULL)
+      if ((list = realloc(list, sizeof *list + (list->alloc_size + LIST_INC_SIZE) * list->el_size)) == NULL)
 	return;
 
       list->alloc_size += LIST_INC_SIZE;
     }
 
-  //memcpy((char *) list->data + (list->count * list->el_size), data, list->el_size);
   memcpy((char *) list + sizeof *list + (list->count * list->el_size), data, list->el_size);
   list->count++;
 }
@@ -103,9 +102,9 @@ void list_add(list_t *list, void *data)
 void list_remove(list_t *list, void *data)
 {
   for (size_t i = list->count; i > 0; i--)
-    if ((char *) list->data + (i - 1) * list->el_size == data)
+    if ((char *) list + sizeof *list + (i - 1) * list->el_size == data)
       for (size_t j = i; j < list->count; j++)
-	memcpy((char*) list->data + (j - 1) * list->el_size, (char *) list->data + j * list->el_size, list->el_size);
+	memcpy((char*) list + sizeof *list + (j - 1) * list->el_size, (char *) list->data + j * list->el_size, list->el_size);
 
   list->count--;
 }
